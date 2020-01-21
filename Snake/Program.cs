cdabb9914e26001ea8f3b63b84ac0d3120410a46
Illay Boc <qwerty1207b2001@gmail.com>
2020-01-21 17:03:59 +0200

    HomeWork
    + List<char> symList
    + List<Point> pList_HW

--- Snake/Program.cs
+++ Snake/Program.cs
@@ -16,6 +16,16 @@ namespace Snake
 			Point p2 = new Point(4, 5, '#');
 			p2.Draw();
 
+		// HomeWork
+			List<char> symList = new List<char>();
+			symList.Add('@');
+			symList.Add('&');
+			symList.Add('$');
+
+			char Q = symList[0];
+			char W = symList[1];
+			char E = symList[2];
+		// HomeWork
 
 			List<int> numList = new List<int>();
 			numList.Add(0);
@@ -33,6 +43,18 @@ namespace Snake
 			
 			numList.RemoveAt(0);
 
+		// HomeWork
+			Point p3 = new Point(x, z, Q);
+			p3.Draw();
+			Point p4 = new Point(y, x, E);
+			p4.Draw();
+
+			List<Point> pList_HW = new List<Point>();
+			pList_HW.Add(p1);
+			pList_HW.Add(p2);
+			pList_HW.Add(p3);
+			pList_HW.Add(p4);
+			// HomeWork
 
 			List<Point> pList = new List<Point>();
 			pList.Add(p1);
